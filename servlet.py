b1eeff9ce6db7c14aa9fc5e129a4464eb0764251
Alex Jones <atjalex@gmail.com>
2016-03-07 19:28:30 -0800

    Added load 100 elements of citation network into graph

--- servlet.py
+++ servlet.py
@@ -1,12 +1,11 @@
 from flask import Flask, jsonify, render_template, request
 from utils import random_network, convert_networkx
+from load_corpus import load_corpus
 app = Flask(__name__)
-
 @app.route("/_get_cy_data")
 def random():
     print "called get_cy_data"
-    r = random_network()
-    c = convert_networkx(r)['elements']
+    c = convert_networkx(global_graph)['elements']
 
     return jsonify(c)
 
@@ -15,4 +14,10 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    global_graph = None
+ 
+    #Load a corpus into memory
+    loader = load_corpus('./datasets/','Cit-HepTh.txt','stanford-hepth',100)
+
+    global_graph = loader.get_graph('stanford-hepth')
     app.run()
